c4375745cfb047b64912ccf0a4dde9321df3b3f8
ValterBarbosa <contato@valterbarbosa.com.br>
2020-08-05 20:59:39 -0300

    Envoy

--- deploy/jenkins/deploy-envsubst.sh
+++ deploy/jenkins/deploy-envsubst.sh
@@ -10,6 +10,7 @@ envsubst < "docker-compose.override.yml" > "docker-compose.override.env.yml";
 envsubst < "docker-compose.prod.yml" > "docker-compose.prod.env.yml";
 
 # docker stack deploy --compose-file ./docker-compose.yml -c ./docker-compose.prod.yml -c ./docker-compose.override.yml eshop
+# docker stack deploy --compose-file ./docker-compose.env.yml -c ./docker-compose.prod.env.yml -c ./docker-compose.override.env.yml eshop
 
 docker-compose -f ./docker-compose.env.yml -f ./docker-compose.override.env.yml -f ./docker-compose.prod.env.yml build
 

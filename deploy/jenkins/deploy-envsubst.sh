e207bb0760d9a784b9b9fc72dd1a7bac7a1f1cea
ValterBarbosa <contato@valterbarbosa.com.br>
2020-08-05 20:16:54 -0300

    Jenkins Deploy -> deploy-envsubst.sh

---
+++ deploy/jenkins/deploy-envsubst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# avoid to use env_file=.env in every service in docker-compose
+
+cd ../../src/
+
+set -a
+source .env
+
+envsubst < "docker-compose.yml" > "docker-compose.env.yml";
+envsubst < "docker-compose.override.yml" > "docker-compose.override.env.yml";
+envsubst < "docker-compose.prod.yml" > "docker-compose.prod.env.yml";
+
+# docker stack deploy --compose-file ./docker-compose.yml -c ./docker-compose.prod.yml -c ./docker-compose.override.yml eshop
+
+docker-compose -f ./docker-compose.env.yml -f ./docker-compose.override.env.yml -f ./docker-compose.prod.env.yml build
+
+rm -rf docker-compose.env.yml docker-compose.override.env.yml docker-compose.prod.env.yml

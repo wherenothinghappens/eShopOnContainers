2efd52720821ff098c883888a221680241b05254
ValterBarbosa <contato@valterbarbosa.com.br>
2020-08-06 15:23:20 -0300

    docker stack deploy -> Jenkinsfile

--- deploy/jenkins/deploy-envsubst.sh
+++ deploy/jenkins/deploy-envsubst.sh
@@ -9,8 +9,8 @@ envsubst < "docker-compose.yml" > "docker-compose.env.yml";
 envsubst < "docker-compose.override.yml" > "docker-compose.override.env.yml";
 # envsubst < "docker-compose.prod.yml" > "docker-compose.prod.env.yml";
 
-# docker stack deploy --compose-file ./docker-compose.yml -c ./docker-compose.prod.yml -c ./docker-compose.override.yml eshop
-# docker stack deploy --compose-file ./docker-compose.env.yml -c ./docker-compose.prod.env.yml -c ./docker-compose.override.env.yml eshop
+# docker stack deploy --compose-file ./docker-compose.yml -c ./docker-compose.override.yml eshop
+# docker stack deploy --compose-file ./docker-compose.env.yml -c ./docker-compose.override.env.yml eshop
 
 docker-compose -f ./docker-compose.env.yml -f ./docker-compose.override.env.yml build # -f ./docker-compose.prod.env.yml 
 

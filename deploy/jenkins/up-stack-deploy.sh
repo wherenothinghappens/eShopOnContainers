2eb03ea829c3b9d9e936a5359942fe9aab042f71
V@LY3E <contato@valterbarbosa.com.br>
2020-08-20 17:30:56 -0300

    Stack Deploy docker-compose.prod.yml

--- deploy/jenkins/up-stack-deploy.sh
+++ deploy/jenkins/up-stack-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker stack rm eshop
+# docker stack rm eshop
 
 # docker stack don't use .env data, just docker-compose can do it
-DOCKER_FILE_FROM_ENV=`docker-compose -f ./docker-compose.yml -f ./docker-compose.override.yml --log-level ERROR config`
+DOCKER_FILE_FROM_ENV=`docker-compose -f ./docker-compose.yml -f ./docker-compose.prod.yml --log-level ERROR config`
 
 echo "$DOCKER_FILE_FROM_ENV" > docker-compose.env.yml && sleep 1
 
 docker stack deploy --compose-file ./docker-compose.env.yml eshop
 
-rm -rf docker-compose.env.yml docker-compose.override.env.yml docker-compose.prod.env.yml
+rm -rf docker-compose.env.yml

79d7d969c264db0e6488c3de99d1029603857899
V@LY3E <contato@valterbarbosa.com.br>
2020-08-20 16:52:34 -0300

    Merge Part 1

--- deploy/jenkins/up-stack-deploy.sh
+++ deploy/jenkins/up-stack-deploy.sh
@@ -2,10 +2,11 @@
 
 docker stack rm eshop
 
-# ESHOP_AZURE_SERVICE_BUS=eshop_rabbitmq :: docker stack prefix "eshop_"
+# ESHOP_AZURE_SERVICE_BUS=eshop_rabbitmq :: docker stack put prefix "eshop_" in all services
 DOCKER_FILE_FROM_ENV=`ESHOP_AZURE_SERVICE_BUS=eshop_rabbitmq docker-compose -f ./docker-compose.yml -f ./docker-compose.override.yml --log-level ERROR config`
 
-sleep 1 && echo "$DOCKER_FILE_FROM_ENV" > docker-compose.env.yml && sleep 1
+# docker stack don't use .env data, just docker-compose can do it
+echo "$DOCKER_FILE_FROM_ENV" > docker-compose.env.yml && sleep 1
 
 docker stack deploy --compose-file ./docker-compose.env.yml eshop
 
